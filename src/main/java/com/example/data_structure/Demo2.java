f2505f1361ef6df1c5568b346e3ce1e4d556729d
Shahrukh.Khan <shahrukh.khan1527@gmail.com>
2025-07-16 22:01:01 +0530

    climb stairs

--- src/main/java/com/example/data_structure/Demo2.java
+++ src/main/java/com/example/data_structure/Demo2.java
@@ -1,7 +1,35 @@
 package com.example.data_structure;
 
 public class Demo2 {
+
+    public static int climbStairs(int n) {
+        if(n==1 || n==0){
+            return 1;
+        }
+        // int[] cs = new int[n];
+        // cs[0]=1;
+        // cs[1]=2;
+
+        // for(int i=2;i<n;i++){
+        //     cs[i] = cs[i-1]+cs[i-2];
+        // }
+
+        // return cs[n-1];
+
+        int prev=1;
+        int curr=2;
+
+        for(int i=3;i<=n;i++){
+            int next = prev+curr;
+            prev=curr;
+            curr=next;
+        }
+
+        return curr;
+
+    }
     public static void main(String[] args) {
-        System.out.println("Testing for PR raise");
+        int result = climbStairs(3);
+        System.out.println(result);
     }
 }
